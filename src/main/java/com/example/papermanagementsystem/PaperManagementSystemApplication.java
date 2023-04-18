d88bf4c6365872ba94d65f1ecf29e1d371037ab5
hhucni <2659367248@qq.com>
2023-04-18 19:48:52 +0800

    init

---
+++ src/main/java/com/example/papermanagementsystem/PaperManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.papermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/papermanagementsystem/PaperManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.papermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
